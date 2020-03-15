ea8b4e85f27ada126c3483b8785c9d6c143b22e4
AshrafMahdhi <44941594+AshrafMahdhi@users.noreply.github.com>
2020-03-16 00:15:38 +0100

    Add files via upload

---
+++ chatup.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app =Flask(__name__)
+
+app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc'
+socketio = SocketIO( app )
+
+@app.route( '/' )
+def hello():
+  return render_template( './ChatApp.html' )
+
+def messageRecived():
+  print( 'message was received!!!' )
+
+
+@socketio.on( 'my eventes' )
+def handle_my_custom_event1( json1 ):
+  import chatbot_final
+  message = json1['message']
+  answer=chatbot_final.chat(message)
+  json1['answer'] = answer
+  json1['bot']='CORONA-VIRUS bot'
+  print( 'recived my event: ' + str(json1 ))
+  socketio.emit( 'my response', json1, callback=messageRecived )
+
+if __name__ == '__main__':
+  socketio.run( app, debug = True , port=5004)
